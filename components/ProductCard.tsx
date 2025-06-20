20f3f1baf8eaed2ac4e1aa9b57effc7bc39d3aff
Sandy <sandy.madanirina@gmail.com>
2025-06-20 23:51:33 +0300

    change component product design.

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -7,7 +7,8 @@ export const ProductCard = ({ product }: { product: any }) => {
   const { addToCart } = useCart()
 
   return (
-    <div className="border p-4 rounded bg-white shadow">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img className="w-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9YL_gULeR_w6Vus30JxsM0lGuiHFHmJKG5Q&s" alt="Sunset in the mountains"></img>
       <h2 className="text-lg font-semibold mb-2">{product.name}</h2>
       <p className="text-gray-600">{product.description}</p>
       <p className="font-bold mt-2">{product.price} Ar</p>
